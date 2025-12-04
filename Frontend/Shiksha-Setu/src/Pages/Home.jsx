80f698312d3e297965e70fcaac0166ec4f052cbf
Harshit Anjana <70934740+harshitaanjna29@users.noreply.github.com>
2025-12-04 05:14:35 +0000

    Added logo and basic home page

--- Frontend/Shiksha-Setu/src/Pages/Home.jsx
+++ Frontend/Shiksha-Setu/src/Pages/Home.jsx
@@ -1,7 +1,20 @@
+import homepageImage from "../assets/homepage.png";
+import "../App.css";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page Loaded</h1>
+    <div className="home-container">
+      <section className="hero-section">
+        <div className="hero-text">
+          <h1>Learning that gets you</h1>
+          <p>Skills for your future. Start your learning journey with Shiksha Setu.</p>
+          <button className="explore-btn">Explore Courses</button>
+        </div>
+
+        <div className="hero-image">
+          <img src={homepageImage} alt="Learning" />
+        </div>
+      </section>
     </div>
   );
 }
