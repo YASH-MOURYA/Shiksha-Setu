0ead78c2938f1a443784fffe67c153ae683f0469
Pranav1403 <86977402+Pranav1403@users.noreply.github.com>
2025-12-11 09:41:26 +0000

    navbar added with my content

--- Frontend/Shiksha-Setu/src/Components/Navbar.jsx
+++ Frontend/Shiksha-Setu/src/Components/Navbar.jsx
@@ -3,39 +3,28 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <>
-      {/* --- YOUR OLD NAVBAR (simple links) --- */}
-      <nav style={{ padding: "10px", background: "#eee", marginBottom: "10px" }}>
-        <a href="/">Home</a> |
-        <a href="/login"> Login </a> |
-        <a href="/register"> Register </a> |
-        <a href="/courses"> Courses </a> |
-        <a href="/contact">Contact </a> | 
-        <a href="/articles">Articles</a>
-      </nav>
+    <nav className="navbar">
+      {/* LEFT */}
+      <div className="nav-left">
+        <Link to="/" className="nav-logo brand-text">
+          Shiksha Setu
+        </Link>
+      </div>
 
-      {/* --- NEW NAVBAR FROM main BRANCH --- */}
-      <nav className="navbar">
-        {/* LEFT */}
-        <div className="nav-left">
-          <Link to="/" className="nav-logo brand-text">
-            Shiksha Setu
-          </Link>
-        </div>
+      {/* CENTER - SEARCH BAR */}
+      <div className="nav-search">
+        <input type="text" placeholder="Search for courses..." />
+      </div>
 
-        {/* CENTER - SEARCH BAR */}
-        <div className="nav-search">
-          <input type="text" placeholder="Search for courses..." />
-        </div>
-
-        {/* RIGHT */}
-        <div className="nav-right">
-          <Link to="/" className="nav-item">Home</Link>
-          <Link to="/login" className="nav-item">Login</Link>
-          <Link to="/register" className="nav-item">Register</Link>
-          <Link to="/courses" className="nav-item">Courses</Link>
-        </div>
-      </nav>
-    </>
+      {/* RIGHT */}
+      <div className="nav-right">
+        <Link to="/" className="nav-item">Home</Link>
+        <Link to="/login" className="nav-item">Login</Link>
+        <Link to="/register" className="nav-item">Register</Link>
+        <Link to="/courses" className="nav-item">Courses</Link>
+        <Link to="/contact" className="nav-item">Contact</Link>
+        <Link to="/articles" className="nav-item">Articles</Link>
+      </div>
+    </nav>
   );
 }
